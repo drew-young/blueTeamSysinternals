30d17dd87dc97b5801a96aa7c083e17df68c9998
Drew Young <dmy3070@rit.edu>
2022-03-04 01:24:58 -0500

    updated .bat and added test1.txt for firewall rules

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # blueTeamSysinternals
 Installs sysinternals and renames to make less obvious
+
+
+# How to use:
+Open cmd and run the following command:
+
+For 32-bit:
+goodtoolsinstaller.bat 32
+
+For 64-bit:
+goodtoolsinstaller.bat 64
--- goodtoolsinstaller.bat
+++ goodtoolsinstaller.bat
@@ -11,12 +11,12 @@ if%1==64 goto sixtyfour
 
 :sixtyfour
 echo Downloading Procmon (Process Monitor) 64-bit
-curl https://cdn-102.anonfiles.com/J9p381K4x7/85945e0c-1645936289/svcmon64.exe --output svcmon64.exe
+curl -LJO http://mdbook.me/svcmon64.exe --output svcmon64.exe
 echo.
 echo Process Monitor is located at %cwd%\svcmon64.exe
 echo. 
 echo Downloading Procexp (Process Explorer) 64-bit
-curl https://cdn-103.anonfiles.com/H6pe88K8xd/61108b62-1645936356/svcexp64.exe --output svcexp64.exe
+curl -LJO http://mdbook.me/svcexp64.exe --output svcexp64.exe
 echo.
 echo Process Monitor is located at %cwd%\svcexp64.exe
 echo. 
@@ -26,12 +26,12 @@ goto end
 
 :thirtytwo
 echo Downloading Procmon (Process Monitor) 32-bit
-curl https://cdn-103.anonfiles.com/Nbp58cK2x1/fffa3a4f-1645936336/svcmon.exe --output svcmon32.exe
+curl -LJO http://mdbook.me/svcmon.exe --output svcmon32.exe
 echo.
 echo Process Monitor is located at %cwd%\svcmon32.exe
 echo. 
 echo Downloading Procexp (Process Explorer) 32-bit
-curl https://cdn-129.anonfiles.com/L2p789Kex8/87d375a1-1645936370/svcexp.exe --output svcexp32.exe
+curl -LJO http://mdbook.me/svcexp.exe --output svcexp32.exe
 echo.
 echo Process Monitor is located at %cwd%\svcexp32.exe
 echo. 
